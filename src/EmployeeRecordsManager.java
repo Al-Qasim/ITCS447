064553f731bc97ec4cef341ca34154d52127e21b
Al-Qasim <43882930+Al-Qasim@users.noreply.github.com>
2018-12-09 14:04:59 +0300

    JFileChooser for saving && More input handling.
    1- Now user can choose where to save the list and what its name is.
    
    2- When any field is empty a record will not be created.
    
    3- When no record is created, list will not be written to file.
    
    4- When no record is selected, an appropriate message is displayed.
    
    !! - The list still cannot be read from file without InputMismatchException on BirthDate - !!
    % A fix is still needed%

--- src/EmployeeRecordsManager.java
+++ src/EmployeeRecordsManager.java
@@ -143,7 +143,7 @@ public class EmployeeRecordsManager extends JFrame implements ActionListener
 
         listModel= new DefaultListModel();
         listInCreation= new JList(listModel);
-        listModel.addElement(String.format("%-20s %-20s %-20s %-20s %-20s %-20s %-20s %20s",
+        listModel.addElement(String.format("%-25s %-25s %-25s %-25s %-25s %-25s %-25s %-25s",
                                             "Employee ID", "First Name","Last Name","Gender", "D.O.B.",
                                             "Department", "Position", "Salary"));
         listInCreation.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -215,88 +215,103 @@ public class EmployeeRecordsManager extends JFrame implements ActionListener
         {
             new EmployeeRecordsManager("");
         }
-        else if(e.getSource()==importExisting)                                                                      // if import button is pressed.
+        else if(e.getSource()==importExisting) // if import button is pressed.
         {
 
             FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Files", "txt"); //extension filter for filechooser.
-            fileChooser.addChoosableFileFilter(filter);                                                             //add filer to filechooser.
-            int status= fileChooser.showOpenDialog(this);                             //save value of selection.
-            if(status== fileChooser.APPROVE_OPTION) {                                                  // open filechooser dialog when import button is pressed.
+            fileChooser.addChoosableFileFilter(filter);//add filer to filechooser.
+            int status= fileChooser.showOpenDialog(this);//save value of selection.
+            if(status== fileChooser.APPROVE_OPTION)// open filechooser dialog when import button is pressed.
+            {
                 opened= fileChooser.getSelectedFile();
                 new EmployeeRecordsManager(opened);
-
             }
         }
 
-        else if(e.getSource()==addEmp)
-        {
+        else if(e.getSource()==addEmp) {
 
-            boolean found = false;
-            char ge;
-            long temp= Long.parseLong(tfID.getText());
-
-            selectedDate= GregorianCalendar.from(datePicker.getDate().atStartOfDay(ZoneId.systemDefault()));
-
-            if(cbGender.getSelectedItem().toString() =="Male")
-                ge='M';
-            else
-                ge='F';
-            float money= Float.parseFloat(tfSal.getText());
-
-            found=listProgress.searchByEmpId(temp);
-            if(found)
-                JOptionPane.showMessageDialog(this, "There is already a list member with the same ID!",
+            if (tfID.getText().equals("") || tfFname.getText().equals("") || tfLname.getText().equals("") ||
+                    datePicker.getText().equals("") || tfSal.getText().equals(""))
+                JOptionPane.showMessageDialog(this, "You must fill in all required fields!",
                         "ERROR", JOptionPane.ERROR_MESSAGE);
-            else{
-            Employee New= null;
-            try {
-                New = new Employee(temp, tfFname.getText(), tfLname.getText(), ge,
-                                        selectedDate, cbDept.getSelectedItem().toString(),
-                                        money, cbPos.getSelectedItem().toString());
-            } catch (InvalidIDException e1) {
-                e1.printStackTrace();
+            else
+                {
+                boolean found;
+                char ge;
+                long temp = Long.parseLong(tfID.getText());
+
+                selectedDate = GregorianCalendar.from(datePicker.getDate().atStartOfDay(ZoneId.systemDefault()));
+
+                if (cbGender.getSelectedItem().toString().equals("Male"))
+                    ge = 'M';
+                else
+                    ge = 'F';
+                float money = Float.parseFloat(tfSal.getText());
+
+                found = listProgress.searchByEmpId(temp);
+                if (found)
+                    JOptionPane.showMessageDialog(this, "There is already a list member with the same ID!",
+                            "ERROR", JOptionPane.ERROR_MESSAGE);
+                else {
+                    Employee New = null;
+                    try {
+                        New = new Employee(temp, tfFname.getText(), tfLname.getText(), ge,
+                                selectedDate, cbDept.getSelectedItem().toString(),
+                                money, cbPos.getSelectedItem().toString());
+                    } catch (InvalidIDException e1) {
+                        e1.printStackTrace();
+                    }
+                    listProgress.addEmployeeEnd(New);
+
+
+
+                String E = String.format("%-25s %-25s %-25s %-25s %-25s %-25s %-25s %-25s",
+                        tfID.getText(), tfFname.getText(), tfLname.getText(),
+                        cbGender.getSelectedItem(), datePicker.getText(),
+                        cbDept.getSelectedItem(), cbPos.getSelectedItem(), tfSal.getText());
+                listModel.addElement(E);}
             }
-            listProgress.addEmployeeEnd(New);
-
-
-
-
-            String E= String.format("%-20s %-20s %-20s %-20s %-20s %-20s %-20s %20s",
-                    tfID.getText(), tfFname.getText(),tfLname.getText(),
-                    cbGender.getSelectedItem(), datePicker.getText(),
-                    cbDept.getSelectedItem(), cbPos.getSelectedItem(), tfSal.getText());
-            listModel.addElement(E);}
         }
 
         else if(e.getSource()==delEmp)
         {
-            if(listInCreation.getSelectedIndex()!=0) {
+            if (listInCreation.isSelectionEmpty())
+                JOptionPane.showMessageDialog(this, "You must select a record to delete!",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            else if(listInCreation.getSelectedIndex()!=0) {
                 listModel.removeElementAt(listInCreation.getSelectedIndex());
                 listProgress.deleteEmployeeAt(listInCreation.getSelectedIndex()+1);
             }
             else
-                JOptionPane.showMessageDialog(this, "There are no more records to delete!",
-                        "ERROR", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(this, "This is not a record of the list!",
+                        "ERROR", JOptionPane.WARNING_MESSAGE);
         }
 
-        else if(e.getSource()==finalize)
-        {
+        else if(e.getSource()==finalize) {
             //do this now.
-            try {
-                writer= new Formatter("testfile.txt");
-                for(int i=0; i< listProgress.size(); i++)
-                {
-                    writer.format("%-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s\n",
-                            listProgress.list[i].getEmpID(), listProgress.list[i].getFirstName(),listProgress.list[i].getLastName(),
-                            listProgress.list[i].getGender(), listProgress.list[i].getBirthDate(),
-                            listProgress.list[i].getDepartment(), listProgress.list[i].getPosition(), listProgress.list[i].getSalary());
+            if(listModel.getSize()==1)
+                JOptionPane.showMessageDialog(this, "There are no records in the list!",
+                        "ERROR", JOptionPane.ERROR_MESSAGE);
+            else{
+                try {
+                    int status = fileChooser.showSaveDialog(this);
+                    if (status == fileChooser.APPROVE_OPTION) {
+                        File save = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                        writer = new Formatter(save);
+                        for (int i = 0; i < listProgress.size(); i++) {
+                            writer.format("%-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s\n",
+                                    listProgress.list[i].getEmpID(), listProgress.list[i].getFirstName(), listProgress.list[i].getLastName(),
+                                    listProgress.list[i].getGender(), listProgress.list[i].getBirthDate(),
+                                    listProgress.list[i].getDepartment(), listProgress.list[i].getPosition(), listProgress.list[i].getSalary());
+                        }
+                        writer.close();
+                    }
+                    }catch(FileNotFoundException e1){
+                    e1.printStackTrace();
                 }
-                writer.close();
-            } catch (FileNotFoundException e1) {
-                e1.printStackTrace();
+
+
             }
-
-
         }
 
     }
